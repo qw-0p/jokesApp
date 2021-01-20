bc69bf2e3065ffaced2a7b1b961d4d728ee6875f
Aleksandr Sarkisov <quopiks@gmail.com>
2021-01-20 15:44:32 +0200

    remove useless return

--- src/components/ModalPage.js
+++ src/components/ModalPage.js
@@ -3,15 +3,13 @@ import { Modal, Button } from 'react-bootstrap';
 
 import { JokeText } from '../styled/ModalPage';
 
-export const ModalPage = ({ show, joke, onHide }) => {
-    return (
-        <Modal show={show} centered size='lg'>
-            <Modal.Body>
-                <JokeText>{joke}</JokeText>
-            </Modal.Body>
-            <Modal.Footer>
-                <Button onClick={() => onHide()}>Close</Button>
-            </Modal.Footer>
-        </Modal>
-    );
-};
+export const ModalPage = ({ show, joke, onHide }) => (
+    <Modal show={show} centered size='lg'>
+        <Modal.Body>
+            <JokeText>{joke}</JokeText>
+        </Modal.Body>
+        <Modal.Footer>
+            <Button onClick={() => onHide()}>Close</Button>
+        </Modal.Footer>
+    </Modal>
+);

2ba49d9e15a6ef89ec2efac906b3b98cd946e1de
Aleksandr Sarkisov <quopiks@gmail.com>
2021-01-28 15:44:01 +0200

    added tests to app state

---
+++ src/redux/ducks/app.test.ts
@@ -0,0 +1,41 @@
+import { IAppState, AppActionTypes } from './appTypes';
+import {reducer, hideLoader, showLoader, SHOW_LOADER, HIDE_LOADER} from './app'
+
+describe('app', () => {
+	describe('actions', () => {
+		it('should create an action showLoader', () => {
+			const expectedAction = {
+			  type: SHOW_LOADER,
+			}
+			expect(showLoader()).toEqual(expectedAction)
+		  })
+		it('should create an action hideLoader', () => {
+			const expectedAction = {
+			  type: HIDE_LOADER,
+			}
+			expect(hideLoader()).toEqual(expectedAction)
+		  })
+	});
+	describe('reducer', () => {
+		let initialState: IAppState = {
+			loading: false
+		}
+		it('should handle SHOW_LOADER', () => {
+			expect(
+			  reducer(initialState, showLoader())
+			).toEqual({loading: true})
+		})
+		it('should handle HIDE_LOADER', () => {
+			expect(
+			  reducer(initialState, hideLoader())
+			).toEqual({loading: false})
+		})
+
+		it('should return state', () => {
+			const testAction = (): any => {
+				return {type: 'TEST'}
+			}
+			expect(reducer(undefined, testAction())).toEqual(undefined)
+		})
+	})
+})
--- src/redux/ducks/app.ts
+++ src/redux/ducks/app.ts
@@ -6,7 +6,7 @@ const initialState: IAppState = {
     loading: false
 };
 
-const reducer = (state = initialState, action: AppActionTypes): IAppState => {
+export const reducer = (state = initialState, action: AppActionTypes): IAppState => {
     switch (action.type) {
         case SHOW_LOADER:
             return { ...state, loading: true };
